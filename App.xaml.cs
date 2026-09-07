f34f3e471585d09276269bd96f3ad6efac6563b4
parvanyik daniel <158039395+CubeGuy0919@users.noreply.github.com>
2026-09-07 11:04:43 +0200

    Add Basics Of The App

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CineTrack___The_Ultimate_Movie___Series_Tracker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
